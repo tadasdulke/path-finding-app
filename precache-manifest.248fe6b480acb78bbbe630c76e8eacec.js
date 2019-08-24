4c7f6ef568208f664f98ca5a0a29134b564a5b18
Tadas552 <tadasdulke5@gmail.com>
2019-08-24 16:33:53 +0300

    Updates

---
+++ precache-manifest.248fe6b480acb78bbbe630c76e8eacec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96af812116e5f55aa8cc7e3278b44708",
+    "url": "/path-finding-app/index.html"
+  },
+  {
+    "revision": "f433ffda7615c8a7f1b2",
+    "url": "/path-finding-app/static/css/main.93aa4331.chunk.css"
+  },
+  {
+    "revision": "0b15b5944e0898fe0229",
+    "url": "/path-finding-app/static/js/2.9b8ce0f9.chunk.js"
+  },
+  {
+    "revision": "f433ffda7615c8a7f1b2",
+    "url": "/path-finding-app/static/js/main.7ecffded.chunk.js"
+  },
+  {
+    "revision": "b3d34aeefee2c1ba5f5c",
+    "url": "/path-finding-app/static/js/runtime~main.f66a6dd8.js"
+  }
+]);
