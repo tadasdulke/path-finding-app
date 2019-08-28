2a364fe2d44a5f125beb8709885d5b38d483d295
Tadas552 <tadasdulke5@gmail.com>
2019-08-28 18:54:48 +0300

    Add existing file

--- src/components/UIStatesComponent.js
+++ src/components/UIStatesComponent.js
@@ -7,37 +7,37 @@ export default function UIStatesComponent() {
         <Container className="w-50 container">
             <p>UI States</p>
             <Row>
-                <Col sm={6} md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>Filled
                         <div className="box box-filled"></div>                    
                     </label>
                 </Col>
-                <Col sm={6} md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>Hover
                         <div className="box box-hover"></div>                    
                     </label>
                 </Col>
-                <Col sm={6} md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>Down
                         <div className="box box-down"></div>                    
                     </label>
                 </Col>
-                <Col sm={6} md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>Clear
                         <div className="box box-clear"></div>                    
                     </label>
                 </Col>
-                <Col sm={6}  md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>Start
                         <div className="box box-start"></div>                    
                     </label>
                 </Col>
-                <Col sm={6} md={2}>
+                <Col xs={6} sm={6} md={2} xl={2}>
                     <label>End
                         <div className="box box-end"></div>                    
                     </label>
                 </Col>
-                <Col sm={6}  md={2}>
+                <Col xs={6} sm={6}  md={2} xl={2}>
                     <label>Shortest Path
                         <div className="box box-shortest"></div>                    
                     </label>
